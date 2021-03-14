6edf43648e150aeee17ceb59ecf67370f5001a81
gonshiban <nss.0120g@gmail.com>
2021-03-15 00:08:57 +0800

    postにおけるエラーの解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  include ActionController::Helpers
+
+  helper_method :login!, :current_user
+
+  def login!
+      session[:user_id] = @user.id
+  end
+
+  def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,5 @@
 class RegistrationsController < ApplicationController
+
   def signup
     @user = User.new(registrations_params)
 
