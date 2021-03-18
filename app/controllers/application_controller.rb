17f367df06ff5ecd12cb2684bdd38698ff3f6130
gonshiban <nss.0120g@gmail.com>
2021-03-18 21:50:38 +0900

    apiモードでのsession周りの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,13 @@
 class ApplicationController < ActionController::API
   include ActionController::Helpers
-
   helper_method :login!, :current_user
 
   def login!
-      session[:user_id] = @user.id
+    session[:user_id] = @user.id
   end
 
   def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,2 @@
-# Rails.application.config.session_store :cookie_store, key: '_backend'
+Rails.application.config.session_store :cookie_store, key: '_backend'
+# Rails.application.config.api_only = false
